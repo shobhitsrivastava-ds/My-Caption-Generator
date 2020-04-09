23cdd1911b04e3b93dccba2de273e48d2780915c
Shobhit Srivastava <44674850+shobhitsrivastava-ds@users.noreply.github.com>
2020-04-10 00:50:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,6 @@ def extract_features(filename):
     model = VGG16()
     model.layers.pop()
     model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
-    #print(model.summary())
     image = load_img(filename, target_size=(224, 224))
     image = img_to_array(image)
     image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
